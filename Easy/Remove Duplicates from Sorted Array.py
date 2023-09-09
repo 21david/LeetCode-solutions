38072474654652225b6a375189225b5899ff90e9
David <david.espinosa.21@gmail.com>
2023-09-09 17:35:37 -0500

    Optimize memory usage
    Improved from O(N) space complexity to O(1). Overall simpler code, and more explanatory comments.
    
    Also traverses the list only once instead of up to twice.

--- Easy/Remove Duplicates from Sorted Array.py
+++ Easy/Remove Duplicates from Sorted Array.py
@@ -1,32 +1,40 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array
+
+# 49 ms, beats 96.32%
+# 14.68 MB, beats 48.72%
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        uniques = []
-        k = 0
-        i = 0
-        while i < len(nums):
-            # check for duplicates
-            while i+1 < len(nums) and nums[i+1] == nums[k]:
+
+        '''
+        One pointer (i) will traverse the whole array looking for duplicates.
+        Another pointer (k) will gradually set the numbers in the beginning
+        as i traverses the array and finds new unique numbers.
+
+        O(N) time complexity
+        O(1) space complexity
+        '''
+
+        i, k = 0, 0
+
+        nums_len = len(nums)
+
+        while i < nums_len:
+            # 'i' will traverse all duplicates of current number
+            while i+1 < nums_len and nums[i+1] == nums[i]:
                 i += 1
-            
-            # at this point, i is at the last duplicate number
-            uniques += [nums[i]]
-            k = i+1
+
+            # at this point, 'i' has traversed all duplicates of the
+            # current number
+
+            # officially set the value at index 'k' and move on to
+            # the next index and number
+            nums[k] = nums[i]
+            k += 1
             i += 1
 
-        # copy uniques back onto original list
-        i = 0
-        for num in uniques:
-            nums[i] = num
-            i += 1
-        
-        return len(uniques)
-            
-
-
-
-
-        
+        return k
