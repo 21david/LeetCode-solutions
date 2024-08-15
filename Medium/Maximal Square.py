3d91730861c283401196011e8b4b81a049e52cb2
David <david.espinosa.21@gmail.com>
2024-08-15 18:03:58 -0500

    Update Maximal Square.py

--- Medium/Maximal Square.py
+++ Medium/Maximal Square.py
@@ -28,3 +28,32 @@ class Solution:
                 max_length = max(max_length, dp[r][c])
 
         return max_length**2
+
+
+'''
+Same solution, but written shorter.
+'''
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        R = len(matrix)
+        C = len(matrix[0])
+        dp = [[0] * C for _ in range(R)]
+        max_len = 0
+        
+        for r, c in product(range(R), range(C)):
+            if matrix[r][c] == '1':
+                try:
+                    sq1 = dp[r-1][c]
+                    sq2 = dp[r-1][c-1]
+                    sq3 = dp[r][c-1]
+                except IndexError:
+                    sq1 = 0
+                
+                dp[r][c] = min(sq1, sq2, sq3) + 1
+                max_len = max(max_len, dp[r][c])
+                
+        return max_len**2
