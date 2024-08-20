2339364f8831b7dd7705e7d31e7aea02719fc080
David <david.espinosa.21@gmail.com>
2024-08-19 22:38:18 -0500

    Update and rename Angle Between Hands of a Clock to Angle Between Hands of a Clock.java

--- Medium/Angle Between Hands of a Clock.java
+++ Medium/Angle Between Hands of a Clock.java
@@ -7,12 +7,13 @@ class Solution {
         // Solved in 15 mins 30 seconds
         
         /*
-            The hour hand will start out exactly on the hour. Then, depending on the minute, it will move (miunutes/60 * 100)% to the next hour. This will tell us how far it is from the north position, and we can calculate the degrees with this.
+            The hour hand will start out exactly on the hour. Then, depending on the minute, it will move (miunutes/60 * 100)% to the next hour. 
+            This will tell us how far it is from the north position, and we can calculate the degrees with this.
             
             To calculate how many degrees the minute hand is from the north position, we can use (minutes/60)*360. 
             
-            When we know how many degrees the hour and minute hands are from the north position, we can subtract the lesser one from the greater one to know the angle. If this angle is greater than 180, we subtract it from 360 and return it, if not, then we just return it.
-        
+            When we know how many degrees the hour and minute hands are from the north position, we can subtract the lesser one from the greater 
+            one to know the angle. If this angle is greater than 180, we subtract it from 360 and return it, if not, then we just return it.
         */
         
         if(hour == 12)
