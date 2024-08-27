9a864e76fd7ff18264e0bc92b0a7161bf930ee53
David <david.espinosa.21@gmail.com>
2024-08-27 11:22:27 -0500

    Update and rename Triangle to Triangle.java

--- Medium/Triangle.java
+++ Medium/Triangle.java
@@ -1,20 +1,19 @@
 //  https://leetcode.com/problems/triangle/
 
+/*
+I will try to copy my approach from 931. Minimum Falling Path Sum
+I will make a triangle matrix that will store the minimum
+sum to the bottom at each location. I will copy the bottom
+row of the input matrix to this matrix, then start from the
+second last row. For each element, I will find the minimum
+of the two elements below, add the current value, and set
+that spot to that summed value. Then, I'll repeat this
+for all values, and the last value should be the minimum
+path sum to the bottom.
+*/
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        /*
-        I will try to copy my approach from 931. Minimum Falling Path Sum
-        I will make a triangle matrix that will store the minimum
-        sum to the bottom at each location. I will copy the bottom
-        row of the input matrix to this matrix, then start from the
-        second last row. For each element, I will find the minimum
-        of the two elements below, add the current value, and set
-        that spot to that summed value. Then, I'll repeat this
-        for all values, and the last value should be the minimum
-        path sum to the bottom.
-        
-        */
-        
         Stack<List<Integer>> lowestSums = new Stack<>();
         
         List<Integer> bottomRow = triangle.get(triangle.size()-1);
@@ -53,6 +52,4 @@ Sample input:
 [[5],[1,3]]
 [[20],[14,6],[1,2,3],[4,5,6,60]]
 [[2],[3,4],[6,5,7],[4,1,8,3]]
-
-
 */
