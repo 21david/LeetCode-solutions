f19daf71c9aab39dbc3ed29f5e963bc6cdfdd049
David <david.espinosa.21@gmail.com>
2024-09-03 18:10:58 -0500

    Create Paint Fence.py

---
+++ Medium/Paint Fence.py
@@ -0,0 +1,32 @@
+'''
+https://leetcode.com/problems/paint-fence
+'''
+
+'''
+Brute force
+TC: (K^N * N)
+Auxiliary SC: O(1)
+'''
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        # See all the possible ways
+        # print(list(product(range(k), repeat=n)).__str__().replace(', (','\n('))
+
+        def is_valid_way(way):
+            color_streak = 0
+            prev_color = None
+            for color in way:
+                if color == prev_color:
+                    color_streak += 1
+                    if color_streak == 3:
+                        return False
+                else:
+                    color_streak = 1
+                    prev_color = color
+            return True
+
+        # Get all possible ways
+        ways = product(range(k), repeat=n)
+
+        # Count how many are valid
+        return sum(1 for way in ways if is_valid_way(way))
