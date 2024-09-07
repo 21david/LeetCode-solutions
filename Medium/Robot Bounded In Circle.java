73b050b03ca881d7b3b1e322eb740ae89205435a
David <david.espinosa.21@gmail.com>
2024-09-07 12:14:51 -0500

    Update and rename Robot Bounded In Circle to Robot Bounded In Circle.java

--- Medium/Robot Bounded In Circle.java
+++ Medium/Robot Bounded In Circle.java
@@ -1,48 +1,43 @@
 //  https://leetcode.com/problems/robot-bounded-in-circle/
 
+/*
+I think there are only 3 cases. If the pattern fits one of these 3 cases,
+the answer is true. If not, the answer is false.
+
+Case 1) The robot ends up at (0,0) after the first iteration
+Case 2) It ends up at (0,0) after 2 iterations
+Case 3) It ends up at (0,0) after 4 iterations
+
+I could try running the instructions, keeping track of the position after
+1, 2, and 4 iterations. If it reaches (0,0) after any of the iterations, 
+return true right away. If it never reaches (0,0), return false.
+*/
+
+// 0 ms, faster than 100%
+// 36.8 mb, less than 90.64%
+// Solved in 24 minutes
 class Solution {
     int xPos, yPos;
     int dir;
     public boolean isRobotBounded(String instructions) {
-        // 0 ms, faster than 100%
-        // 36.8 mb, less than 90.64%
-        // Solved in 24 minutes
-        
-        /*
-        I think there are only 3 cases. If the pattern fits one of these 3 cases,
-        the answer is true. If not, the answer is false.
-        
-        Case 1) The robot ends up at (0,0) after the first iteration
-        Case 2) It ends up at (0,0) after 2 iterations
-        Case 3) It ends up at (0,0) after 4 iterations
-        
-        I could try running the instructions, keeping track of the position after
-        1, 2, and 4 iterations. If it reaches (0,0) after any of the iterations, 
-        return true right away. If it never reaches (0,0), return false.
-        */
-        
         // 0 is north, 1 is east, 2 is south, 3 is west
         int dir = 0; 
-        
         
         // Execute them 1 time
         execute(instructions);
         if(xPos == 0 && yPos == 0)
             return true;
         
-        
         // Execute them 2 times total
         execute(instructions);
         if(xPos == 0 && yPos == 0)
             return true;
 
-        
         // Execute them 4 times total
         execute(instructions);
         execute(instructions);
         if(xPos == 0 && yPos == 0)
             return true;
-        
         
         return false;
     }
