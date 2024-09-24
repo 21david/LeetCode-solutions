1a0ee828694105f26186a8c46bb381890b749067
David <david.espinosa.21@gmail.com>
2024-09-24 18:01:58 -0500

    Update and rename Longest Valid Parentheses to Longest Valid Parentheses.java

--- Hard/Longest Valid Parentheses.java
+++ Hard/Longest Valid Parentheses.java
@@ -1,26 +1,24 @@
 //  https://leetcode.com/problems/longest-valid-parentheses/
 
+/*
+We can use the normal algorithm that checks whether a string of parenthesis is 
+valid, but with a few changes. We can make a boolean array the same size of the
+input string, and initially set all elements to false. We can make a stack that will
+store '('s, and another stack that stores the index of all the '('s we find. Whenever
+we find a ')' with a matching '(' in the stack, then we know the index of both the
+'(' and ')' because of the other stack we have. In the boolean array, we can set both
+the element at those indices to true. This will be an O(N) operation.
+At the end, we will have a boolean array that knows all of the valid parenthesis and 
+all of the invalid parenthesis. We can iterate through this array, keeping track of the
+lengths of the valid substrings, and looking out for the maximum length one. This is
+O(N) again.
+
+2 ms, faster than 68.47%
+38.9 mb, less tan 56.64%
+Solved in ~30 minutes
+*/
 class Solution {
     public int longestValidParentheses(String s) {
-        // 2 ms, faster than 68.47%
-        // 38.9 mb, less tan 56.64%
-        // Solved in ~30 minutes
-        
-        /*
-        We can use the normal algorithm that checks whether a string of parenthesis is 
-        valid, but with a few changes. We can make a boolean array the same size of the
-        input string, and initially set all elements to false. We can make a stack that will
-        store '('s, and another stack that stores the index of all the '('s we find. Whenever
-        we find a ')' with a matching '(' in the stack, then we know the index of both the
-        '(' and ')' because of the other stack we have. In the boolean array, we can set both
-        the element at those indices to true. This will be an O(N) operation.
-        At the end, we will have a boolean array that knows all of the valid parenthesis and 
-        all of the invalid parenthesis. We can iterate through this array, keeping track of the
-        lengths of the valid substrings, and looking out for the maximum length one. This is
-        O(N) again.
-        
-        */
-        
         boolean[] validOrNot = new boolean[s.length()];
         Arrays.fill(validOrNot, false);
         
@@ -50,7 +48,6 @@ class Solution {
             }
         }
         
-        
         // now we have to find the largest amount of consecutive 'true's in the boolean array
         int max = 0;
         int curInterval = 0;
@@ -79,5 +76,4 @@ Sample input:
 "())("
 ")()())(()()())("
 ")()((())("
-
 */
