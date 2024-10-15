cde0c7905328519cd5bd23b3870cb45de544f589
David <david.espinosa.21@gmail.com>
2024-10-14 21:25:21 -0500

    Update Average Selling Price.sql

--- Easy/Average Selling Price.sql
+++ Easy/Average Selling Price.sql
@@ -12,3 +12,12 @@ union
 select product_id, 0 
 from prices
 where product_id not in (select product_id from unitssold)
+
+
+# After reading solutions: They can be joined on the dates with 'between', and ifnull can turn products not sold into an average of 0.
+select p.product_id, ifnull(round(sum(p.price * u.units) / sum(u.units), 2), 0) as average_price 
+from prices p
+left join unitssold u
+on p.product_id = u.product_id
+    and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
