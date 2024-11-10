0d86e98e4f4c2229f84853e486462e8e9039c47d
David <david.espinosa.21@gmail.com>
2024-11-09 22:45:56 -0600

    Update and rename Best Time to Buy and Sell Stock II to Best Time to Buy and Sell Stock II.java

--- Easy/Best Time to Buy and Sell Stock II.java
+++ Easy/Best Time to Buy and Sell Stock II.java
@@ -1,18 +1,13 @@
 /*
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
-*/
 
-/*
 Initialize a counter variable to store the profit.
-
 Upon start, continue iterating until a minimum is reached. (meaning, until the next number is greater than current number)
 Buy that minimum, then continue iterating until a maximum is reached. 
 When a maximum is reached, sell it, and continue iterating until a minimum is reached.
 Repeat this for the whole array. Stop when 'i' reaches the end of the array.
-
 Return the profit made.
 */
-
 class Solution {
     public int maxProfit(int[] prices) {
         if(prices.length == 0)
@@ -63,17 +58,13 @@ class Solution {
     }
     
     // my solution after seeing the official solution to this problem
+    // 1 ms, faster than 91.24%
     public int maxProfit2(int[] prices) {
-        // 1 ms, faster than 91.24%
-        
         int profit = 0;
         
         for(int i = 0; i < prices.length - 1; i++)
-        {
             if(prices[i + 1] > prices[i])
                 profit += prices[i + 1] - prices[i];
-        
-        }
         
         return profit;
     }
