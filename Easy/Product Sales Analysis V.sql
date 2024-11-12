3f16d05b106c424cc6fa5d885be57a957ff545c1
David <david.espinosa.21@gmail.com>
2024-11-12 15:40:55 -0600

    Update Product Sales Analysis V.sql

--- Easy/Product Sales Analysis V.sql
+++ Easy/Product Sales Analysis V.sql
@@ -4,7 +4,7 @@ select
     user_id, 
     sum(quantity * price) as spending 
 from sales s
-left join product p
+join product p
 on s.product_id = p.product_id
 group by user_id
 order by spending desc, user_id
