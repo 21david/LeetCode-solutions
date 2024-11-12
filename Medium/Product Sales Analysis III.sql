1256c6b6bee3c2b297cdae51d3f9a3e0a7468993
David <david.espinosa.21@gmail.com>
2024-11-12 15:19:27 -0600

    Update Product Sales Analysis III.sql

--- Medium/Product Sales Analysis III.sql
+++ Medium/Product Sales Analysis III.sql
@@ -14,3 +14,17 @@ left join (
     select * from sales
 ) as original
 on mins.product_id = original.product_id and mins.first_year = original.year
+
+
+
+# Another approach, after seeing the editorial:
+# select necessary rows from sales
+# where (product_id, year) is in (sub query that gets first year for each product)
+
+select product_id, year as first_year, quantity, price
+from sales
+where (product_id, year) in (
+    select product_id, min(year)
+    from sales
+    group by 1
+)
