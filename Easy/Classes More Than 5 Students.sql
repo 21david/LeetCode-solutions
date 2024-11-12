19fad858d3f59011e21f99678e96ad25223410b3
David <david.espinosa.21@gmail.com>
2024-11-12 14:20:06 -0600

    Update Classes More Than 5 Students.sql

--- Easy/Classes More Than 5 Students.sql
+++ Easy/Classes More Than 5 Students.sql
@@ -1,8 +1,6 @@
 # https://leetcode.com/problems/classes-more-than-5-students
 
-with counts as (
-    select class, count(student) as count from courses
-    group by 1
-)
-select class from counts
-where count >= 5
+select class from courses
+group by class
+having count(student) >= 5
+
