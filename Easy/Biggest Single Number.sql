61978b26b34109ca933374521910cc9a560f72e8
David <david.espinosa.21@gmail.com>
2024-11-12 14:46:33 -0600

    Update Biggest Single Number.sql

--- Easy/Biggest Single Number.sql
+++ Easy/Biggest Single Number.sql
@@ -11,3 +11,19 @@ with singles as (
 # Get the highest one (returns null if table above is empty)
 select max(num) as num
 from singles
+
+
+# Another approach
+# Get all single numbers
+select num
+from mynumbers
+group by num
+having count(*) = 1
+
+# Add null in case there are no single numbers
+union
+select null
+
+# Take the top one
+order by num desc
+limit 1
