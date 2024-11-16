ca5855f3eb4dfa2438b39e324f57d280e8901545
David <david.espinosa.21@gmail.com>
2024-11-15 21:24:03 -0600

    Optimize and shorten Exchange Seats.sql

--- Medium/Exchange Seats.sql
+++ Medium/Exchange Seats.sql
@@ -2,13 +2,11 @@
 
 select 
     case 
-        when id = (
-            select id from seat
-            order by id desc
-            limit 1
-        ) and id % 2 = 1 then id 
+        when id % 2 = 1 and id = (
+            select count(id) from seat
+        ) then id 
         when id % 2 = 1 then id + 1
-        when id % 2 = 0 then id - 1
+        else id - 1
     end as id,
     student
 from seat
