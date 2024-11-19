0b0de186ca0d21d650d17ec1e70e3fec11c34c11
David <david.espinosa.21@gmail.com>
2024-11-19 11:58:32 -0500

    Update and rename Complement of Base 10 Integer to Complement of Base 10 Integer.java

--- Easy/Complement of Base 10 Integer.java
+++ Easy/Complement of Base 10 Integer.java
@@ -3,14 +3,12 @@ https://leetcode.com/problems/complement-of-base-10-integer/
 
 This problem is the same as #476:   leetcode.com/problems/number-complement
 See my solutions for that one:   github.com/21david/More-LeetCode-problems/blob/master/Easy/Number%20Complement
-*/
 
+1 ms, faster than 27.31%
+35.9 mb, less than 11.11%
+*/
 class Solution {
     public int bitwiseComplement(int N) {
-        
-        // 1 ms, faster than 27.31%
-        // 35.9 mb, less than 11.11%
-        
         String nString = Integer.toBinaryString(N);
         
         int len = nString.length();
