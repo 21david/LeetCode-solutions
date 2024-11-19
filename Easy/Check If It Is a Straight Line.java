f66b73cb3bdb6e63d38e0a7fbebe9d0ed0d93c6b
David <david.espinosa.21@gmail.com>
2024-11-19 11:57:38 -0500

    Update and rename Check If It Is a Straight Line to Check If It Is a Straight Line.java

--- Easy/Check If It Is a Straight Line.java
+++ Easy/Check If It Is a Straight Line.java
@@ -1,27 +1,21 @@
 /*
 https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3323/
-
 May Leetcoding challenge, day 8
-*/
 
+1. Sort by x coordinate (if not already sorted)
+2. check slope between coordinates[0] and coordinates[1]
+  - if this slope is steadily mainteined between every 2 neighboring pairs
+    then it is a straight line
+  - if it is ever broken, then it is not a straight line
+
+0 ms, faster than 100%
+39.3 mb, less than 100%
+*/
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-        // 0 ms, faster than 100%
-        // 39.3 mb, less than 100%
-        
-        /*
-        1. Sort by x coordinate (if not already sorted)
-        2. check slope between coordinates[0] and coordinates[1]
-          - if this slope is steadily mainteined between every 2 neighboring pairs
-            then it is a straight line
-          - if it is ever broken, then it is not a straight line
-        */
-        
         double startingSlope = getSlope(coordinates[0], coordinates[1]);
         double slope;
-        
         boolean startingSlopeBroken = false;
-        
         int i = 1;
         
         while(!startingSlopeBroken && i < coordinates.length - 1)
