527d7d663e8fc846337ca0195c733db5296e8f10
David <david.espinosa.21@gmail.com>
2024-11-19 22:51:06 -0500

    Update Delete Duplicate Emails.sql

--- Easy/Delete Duplicate Emails.sql
+++ Easy/Delete Duplicate Emails.sql
@@ -4,3 +4,17 @@ delete p1
 from person p1
 join person p2
 where p1.email = p2.email and p1.id > p2.id
+
+
+
+# Using a subquery:
+delete from person
+where id not in (
+    # Get the minimum id in each group - the ones to be kept
+    # Nested select is required to avoid error due to an SQL optimization reason
+    select * from (
+        select min(id)
+        from person
+        group by email
+    ) as mins
+)
