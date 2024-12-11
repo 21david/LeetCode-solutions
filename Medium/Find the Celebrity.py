668f8d9231957e03a70312bb0c8b9ace734b13cb
David <david.espinosa.21@gmail.com>
2024-12-11 11:45:18 -0600

    Create Find the Celebrity.py

---
+++ Medium/Find the Celebrity.py
@@ -0,0 +1,46 @@
+'''
+https://leetcode.com/problems/find-the-celebrity
+
+After reading editorial, except code:
+Logical deduction. With every call, you can eliminate at least
+one node. The celebrity is known by everyone and knows no one
+therefore:
+    If a knows b, a can't be the celebrity
+    If a doesn't know b, b can't be the celebrity
+
+In one pass, we can eliminate everyone who is not a celebrity, leaving us
+with one person who may or may not be a celebrity. So we check if they
+are the celebrity or not with two more passes.
+'''
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        possible = 0
+
+        for i in range(1, n):
+            if knows(possible, i):
+                # If current person knows someone, they are NOT the celebrity
+                # But that other person may be
+                possible = i
+
+        # If we found the celebrity at any point, he would stay stuck in possible
+        # because he would know no one. But it's also possible that we just got
+        # someone who knew people that we didn't ask about. So we should check if
+        # possible is actually the celebrity by checking all his incoming and outgoing
+        # edges
+        
+        if all(
+            i == possible or not knows(possible, i) 
+            for i in range(n)
+            ) and all(
+                knows(i, possible)
+                for i in range(n)
+            ):
+            return possible
+        else:
+            return -1
+
+
+        
