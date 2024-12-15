73a37dd0a24bf2c34d26d3011962ee08c07645fe
David <david.espinosa.21@gmail.com>
2024-12-15 06:52:45 -0600

    Create Maximum Average Pass Ratio.py

---
+++ Medium/Maximum Average Pass Ratio.py
@@ -0,0 +1,35 @@
+'''
+https://leetcode.com/problems/maximum-average-pass-ratio
+
+After seeing all hints
+
+TC: O(E log C)
+SC: O(C)
+E = extraStudents
+C = number of classes
+'''
+from heapq import heapify, heappop as pop, heappush as push
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+      # Sort heap by classes that would get the biggest change with 1 passing student
+        changes = [
+            [-(((pair[0]+1) / (pair[1]+1)) - (pair[0] / pair[1])), idx] 
+            for idx, pair in enumerate(classes)
+        ]
+        heapify(changes)
+
+        # Distribute the students optimally
+        while (extraStudents := extraStudents - 1) >= 0:
+            biggest_change, idx = pop(changes)
+
+            # Add student to class i
+            classes[idx][0] += 1
+            classes[idx][1] += 1
+
+            # Push back in case we want to add another student to this class again
+            passing, total = classes[idx]
+            push(changes, [-(((passing+1) / (total+1)) - (passing / total)), idx])
+
+
+        # Return the new average
+        return sum(passing / total for passing, total in classes) / len(classes)
