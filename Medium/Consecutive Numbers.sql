d6119e845ef1a4a3619c34064e470ca716e979e5
David <david.espinosa.21@gmail.com>
2025-01-07 22:01:14 -0600

    Create Consecutive Numbers.sql

---
+++ Medium/Consecutive Numbers.sql
@@ -0,0 +1,7 @@
+# O(N^3) time complexity
+select distinct a.num as ConsecutiveNums
+from logs a
+cross join logs b
+cross join logs c
+where a.num = b.num and a.num = c.num 
+    and a.id + 1 = b.id and a.id + 2 = c.id
