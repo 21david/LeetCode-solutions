1ef7239ebf33aef05af675ba7c454553d8ab06bc
David <david.espinosa.21@gmail.com>
2025-01-07 22:03:05 -0600

    Update Consecutive Numbers.sql

--- Medium/Consecutive Numbers.sql
+++ Medium/Consecutive Numbers.sql
@@ -5,3 +5,10 @@ cross join logs b
 cross join logs c
 where a.num = b.num and a.num = c.num 
     and a.id + 1 = b.id and a.id + 2 = c.id
+
+
+# Equivalent syntactic sugar
+select distinct a.num as ConsecutiveNums
+from logs a, logs b, logs c
+where a.num = b.num and a.num = c.num 
+    and a.id + 1 = b.id and a.id + 2 = c.id
