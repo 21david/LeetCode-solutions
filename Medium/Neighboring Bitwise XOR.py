65d72726caa2e6e5e3f81ca1cf18f26558d01dbd
David <david.espinosa.21@gmail.com>
2025-01-17 14:51:53 -0500

    Create Neighboring Bitwise XOR.py

---
+++ Medium/Neighboring Bitwise XOR.py
@@ -0,0 +1,36 @@
+'''
+After seeing NeetCode's solution video except the code
+'''
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        curr_orig = 0
+
+        for i in range(len(derived) - 1):
+            curr_derived = derived[i]
+
+            if curr_derived == 1:
+                # Numbers in original were different
+                curr_orig = 1 - curr_orig
+            else:
+                # Numbers in original were the same
+                pass
+
+        
+        if derived[-1] == 1:
+            return curr_orig == 1  # 1 ^ 0 == 1
+        else:
+            return curr_orig == 0
+        
+    '''
+    [1, 1, 0]
+    [0, 1, 0]
+    '''
+
+
+# Shortened version
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        curr_orig = 0
+        for curr_derived in derived[:-1]:
+            curr_orig = abs(curr_derived - curr_orig)
+        return curr_orig == (derived[-1] == 1)
