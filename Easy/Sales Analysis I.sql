07dd9eb2e3a5438feb02036537ac67d111e2325c
David <david.espinosa.21@gmail.com>
2025-01-31 18:52:16 -0600

    Update Sales Analysis I.sql

--- Easy/Sales Analysis I.sql
+++ Easy/Sales Analysis I.sql
@@ -1,3 +1,21 @@
+# using group by
+select
+    seller_id
+from sales
+group by seller_id
+# Get the sellers that sold the max number of total price
+having sum(price) = (
+    # Get the maximum sum
+    select
+        sum(price)
+    from sales
+    group by seller_id
+    order by 1 desc
+    limit 1
+);
+
+
+
 # using window function
 with accumulated_sums_table as (
     select
