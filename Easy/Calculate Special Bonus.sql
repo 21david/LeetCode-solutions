8841d395e408aec79aeadd23ee269dc82d01d187
David <david.espinosa.21@gmail.com>
2025-02-09 12:45:21 -0600

    Update Calculate Special Bonus.sql

--- Easy/Calculate Special Bonus.sql
+++ Easy/Calculate Special Bonus.sql
@@ -18,3 +18,15 @@ select
     end as bonus
 from employees
 order by employee_id
+
+
+
+# or
+select 
+    employee_id,
+    case
+        when employee_id % 2 = 1 and name not regexp '^M.*' then salary
+        else 0
+    end as bonus
+from employees
+order by employee_id
