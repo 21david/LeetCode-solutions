f7d0876be7653c27e8f3e0bab88131386bbd5662
David <david.espinosa.21@gmail.com>
2025-02-09 12:44:37 -0600

    Create Calculate Special Bonus.sql

---
+++ Easy/Calculate Special Bonus.sql
@@ -0,0 +1,20 @@
+select 
+    employee_id,
+    case
+        when employee_id % 2 = 1 and name not like 'M%' then salary
+        else 0
+    end as bonus
+from employees
+order by employee_id
+
+
+
+# Using regular expressions
+select 
+    employee_id,
+    case
+        when employee_id % 2 = 1 and name regexp '^[^M].*' then salary
+        else 0
+    end as bonus
+from employees
+order by employee_id
