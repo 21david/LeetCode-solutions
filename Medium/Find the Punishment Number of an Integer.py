a0975c00baad846983b7ff3c06941bf3506f6e2c
David <david.espinosa.21@gmail.com>
2025-02-14 19:49:46 -0600

    Update Find the Punishment Number of an Integer.py

--- Medium/Find the Punishment Number of an Integer.py
+++ Medium/Find the Punishment Number of an Integer.py
@@ -29,6 +29,8 @@ class Solution:
 
 
 # Precomputed numbers, assuming 1 <= n <= 1000
+# TC: O(1)
+# SC: O(1)
 class Solution:
     def punishmentNumber(self, N: int) -> int:
         can_be_partitioned = [1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000]
@@ -42,19 +44,13 @@ class Solution:
         return ans
 
 
-'''
-Another solution would be to precompute the actual outputs for each input,
-and use a set of if-statements like
-if 1 <= n < 9:
-    return 1
-elif n == 9:
-    return 82
-elif 10 <= n < 36:
-    return 182
-elif 36 <= n < 45:
-    return 1478
-...
 
-This may be even faster than the solution above.
-'''
+# Precomputed answers + quick retrieval with binary search
+# TC: O(1)
+# SC: O(1)
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        input = [1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000]
+        output = [0, 1, 82, 182, 1478, 3503, 6528, 13252, 21533, 31334, 41334, 96559, 184768, 320929, 457829, 601470, 772866, 1204515, 1660140, 2154349, 2725885, 3353149, 4179430, 5022154, 5915179, 6844475, 7824575, 8806656, 9804657, 10804657]
+        return output[bisect.bisect(input, n)]
 
