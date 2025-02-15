0ed2e641176ff540884c2ecec5abed1bf3e8bc8d
David <david.espinosa.21@gmail.com>
2025-02-14 19:17:59 -0600

    Update Find the Punishment Number of an Integer.py

--- Medium/Find the Punishment Number of an Integer.py
+++ Medium/Find the Punishment Number of an Integer.py
@@ -25,3 +25,18 @@ class Solution:
                 ans += n ** 2
 
         return ans
+
+
+
+# Precomputed numbers, assuming 1 <= n <= 1000
+class Solution:
+    def punishmentNumber(self, N: int) -> int:
+        can_be_partitioned = [1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000]
+
+        ans = 0
+        for num in can_be_partitioned:
+            if num > N:
+                break
+            ans += num ** 2
+
+        return ans
