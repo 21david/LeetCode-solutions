b2df3354b81ce51549685a0ec059c52eceacd319
David <david.espinosa.21@gmail.com>
2025-03-31 22:28:15 -0500

    Update Solving Questions With Brainpower.py

--- Medium/Solving Questions With Brainpower.py
+++ Medium/Solving Questions With Brainpower.py
@@ -13,3 +13,22 @@ class Solution:
             return max(solve, skip)
 
         return rec(0)
+
+
+# After seeing editorial
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        Q = len(questions)
+
+        dp = [question[0] for question in questions]
+        for i in range(Q-2, -1, -1):
+            next_idx = i + questions[i][1] + 1
+            solve = dp[i]
+            if next_idx < Q:
+                solve += dp[next_idx]
+
+            skip = dp[i+1]
+
+            dp[i] = max(solve, skip)
+
+        return dp[0]
