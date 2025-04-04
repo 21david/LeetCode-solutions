1f36ca26083bbed632e233950d32750cf855b123
David <david.espinosa.21@gmail.com>
2025-04-03 21:13:17 -0500

    Create Lowest Common Ancestor of Deepest Leaves.ts

---
+++ Medium/Lowest Common Ancestor of Deepest Leaves.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+/* 
+After seeing editorial a few days ago:
+Recursive solution with dfs. Always bubble up the max depth and LCA with each return.
+For the dfs call of a node, 
+If left subtree has greater depth, use the LCA that was bubbled up.
+Same for right subtree.
+If depths of left and right are equal, current node is LCA, so return that.
+Base case: a null has a depth of 0 and  no LCA.
+
+TC: O(N) each node visited once
+SC: O(N) call stack reaches height of tree.
+ */
+type Node = TreeNode | null;
+type DFS = (Node) => [number, Node];  // [depth, LCA node]
+function lcaDeepestLeaves(root: Node): Node {
+    const dfs: DFS = (node) => {
+        if (node === null)
+            return [0, null];
+        
+        let [left_depth, left_lca] = dfs(node.left);
+        let [right_depth, right_lca] = dfs(node.right);
+
+        if (left_depth > right_depth)
+            return [left_depth + 1, left_lca];
+        else if (right_depth > left_depth)
+            return [right_depth + 1, right_lca];
+        else
+            return [left_depth + 1, node];
+    }
+
+    return dfs(root)[1];
+};
