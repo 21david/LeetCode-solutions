e2ef9e986bb4bd83f9c6d1d9406d7d4138d73523
David <david.espinosa.21@gmail.com>
2025-06-07 11:02:56 -0500

    Optimize and cleanup

--- Python/Medium/Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values.py
+++ Python/Medium/Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxSumDistinctTriplet(self, x: List[int], y: List[int]) -> int:
-        arr = [(y[i], x[i], i) for i in range(len(x))]
-        arr.sort(reverse=True)
-        seen = set()
-
+        N = len(x)
         maxes = {}
-        for yv, xv, i in arr:
-            if xv in maxes:
-                maxes[xv] = max(maxes[xv], yv)
+        for i in range(N):
+            if x[i] in maxes:
+                maxes[x[i]] = max(maxes[x[i]], y[i])
             else:
-                maxes[xv] = yv
+                maxes[x[i]] = y[i]
 
-        helper = [myv for xv, myv in maxes.items()]
-        helper.sort(reverse=True)
-        
-        if len(helper) < 3: 
+        if len(maxes) < 3:
             return -1
-        return sum(helper[:3])
-©leetcode
+
+        return sum(sorted(maxes.values(), reverse=True)[:3])
