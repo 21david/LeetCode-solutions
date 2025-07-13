730aeaa63b5591bfb505a43d5210436ce2f0676f
David <david.espinosa.21@gmail.com>
2025-07-12 23:30:30 -0500

    Update Process String with Special Operations I.java

--- Java/Medium/Process String with Special Operations I.java
+++ Java/Medium/Process String with Special Operations I.java
@@ -28,3 +28,33 @@ class Solution {
         return answer.toString();
     }
 }
+
+
+// Solution 2
+class Solution {
+    public String processStr(String s) {
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            final boolean empty = answer.length() == 0;
+
+            switch ((Character) c) {
+                // Java 21 syntax
+                case Character ch when Character.isAlphabetic(ch) -> answer.append(ch);
+
+                case Character ch when !empty && ch == '*' -> 
+                    answer.deleteCharAt(answer.length() - 1);
+
+                case Character ch when !empty && ch == '#' -> 
+                    answer.append(answer);
+
+                case Character ch when !empty && ch == '%' -> 
+                    answer.reverse();
+
+                default -> {}
+            }
+        }
+
+        return answer.toString();
+    }
+}
