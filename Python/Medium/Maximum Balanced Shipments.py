d1c40873ad33a3616cd729f4e44bf7d6c0d5ac1a
David <david.espinosa.21@gmail.com>
2025-08-03 00:09:56 -0500

    Optimize space complexity to O(1)

--- Python/Medium/Maximum Balanced Shipments.py
+++ Python/Medium/Maximum Balanced Shipments.py
@@ -1,28 +1,28 @@
-# TC = SC = O(N)
+# TC = O(N)
+# SC = O(1)
 class Solution:
     def maxBalancedShipments(self, weight: List[int]) -> int:
-        # Greedily start with the next two elements, and expand until current shipment meets the conditions
-
-        count = 0
-        curr = []
+        # Greedily start with the next two elements, expand until 
+        # current shipment meets the conditions then restart and repeat
+        answer = 0
+        shipment_length = 0
         curr_max = -math.inf
         for i in range(len(weight)):
-            if len(curr) < 2:
-                curr.append(weight[i])
+            if shipment_length < 2:
+                shipment_length += 1
                 curr_max = max(curr_max, weight[i])
                 continue
 
-            if curr[-1] != curr_max:
+            if weight[i-1] != curr_max:
                 # found shipment that meets condition
-                count += 1
-                curr = []
+                answer += 1
                 curr_max = -math.inf
+                shipment_length = 1
 
-            curr.append(weight[i])
             curr_max = max(curr_max, weight[i])
 
-        if curr[-1] != curr_max:
+        if weight[-1] != curr_max:
             # for the last set of elements
-            count += 1
+            answer += 1
 
-        return count
+        return answer
