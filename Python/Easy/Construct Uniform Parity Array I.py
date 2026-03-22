5b734eb53691065e7fd38f783e0963eddaeb82cd
David <david.espinosa.21@gmail.com>
2026-03-21 23:16:38 -0500

    Create Construct Uniform Parity Array I.py

---
+++ Python/Easy/Construct Uniform Parity Array I.py
@@ -0,0 +1,8 @@
+'''
+See my explanation article:
+https://leetcode.com/problems/construct-uniform-parity-array-i/solutions/7679295/oddeven-logic-by-david1121-ev8m/
+'''
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
