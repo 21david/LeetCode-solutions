97a19951ef89c595a7d920e3796a4aad030908b3
David <david.espinosa.21@gmail.com>
2026-04-02 16:29:51 -0500

    Add function to find actor-director pairs with 3+ films

---
+++ Pandas/Actors and Directors Who Cooperated At Least Three Times.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Solution using regular Python
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    # Make a dictionary to count times each actor-director pair worked together
+    counts = defaultdict(int)
+    for i, row in actor_director.iterrows():
+        counts[(row['actor_id'], row['director_id'])] += 1
+
+    # Filter out pairs where they worked together at least 3 times
+    new_rows = {'actor_id': [],
+                'director_id': []}
+    for actor, director in counts:
+        if counts[actor, director] >= 3:
+            # This pair of actor and director has worked together 
+            # at least 3 times, so add to the result dictionary
+            new_rows['actor_id'].append(actor)
+            new_rows['director_id'].append(director)
+
+    # Put the actor-director pairs in a new dataframe
+    res = pd.DataFrame(new_rows)
+
+    return res
