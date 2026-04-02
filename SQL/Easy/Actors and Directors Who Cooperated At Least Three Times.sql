3f11ddd7b2f947e1eca43d6594198a540cbadae1
David <david.espinosa.21@gmail.com>
2026-04-02 16:45:03 -0500

    Update Actors and Directors Who Cooperated At Least Three Times.sql

--- SQL/Easy/Actors and Directors Who Cooperated At Least Three Times.sql
+++ SQL/Easy/Actors and Directors Who Cooperated At Least Three Times.sql
@@ -1,3 +1,11 @@
+# With GROUP BY and HAVING
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(timestamp) >= 3
+
+
+# With a derived table
 select grouped.actor_id, grouped.director_id 
 from 
     (select actor_id, director_id, count(*) as ct
