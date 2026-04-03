c61bd2fc49e98ae5c49db5eca0444c336f907111
David <david.espinosa.21@gmail.com>
2026-04-03 13:42:12 -0500

    Add alternative approach to find_customer_referee

--- Pandas/Find Customer Referee.py
+++ Pandas/Find Customer Referee.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
     return customer.loc[(customer['referee_id'] != 2) | (customer['referee_id'].isna()), 'name'].to_frame()
+
+
+# Another approach
+# Convert nulls to 0 then just check for != 2, and select a list of 1 column to get back a data frame, instead of converting later
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    customer['referee_id'] = customer['referee_id'].fillna(0)
+    return customer.loc[customer['referee_id'] != 2, ['name']]
