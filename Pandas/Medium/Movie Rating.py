f3abc244e66a0f2e8da1ce52d21be550719cd6ac
David <david.espinosa.21@gmail.com>
2026-04-04 18:30:50 -0500

    Create Movie Rating.py

---
+++ Pandas/Medium/Movie Rating.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def movie_rating(movies: pd.DataFrame, users: pd.DataFrame, movie_rating: pd.DataFrame) -> pd.DataFrame:
+    # Find the name of the user who has rated the greatest number of movies.
+    review_counts = movie_rating.groupby('user_id', as_index=False).size()  # Find review amounts
+    review_counts = review_counts.merge(users)  # Add names column
+    # In case of a tie, return the lexicographically smaller user name.
+    review_counts = review_counts.sort_values(['size', 'name'], ascending=[False, True])
+    first_ans = review_counts.head(1)['name']
+
+    # Find the movie name with the highest average rating in February 2020
+    movie_rating = movie_rating[(movie_rating.created_at >= '2020-02-01') & (movie_rating.created_at <= '2020-02-29')]  # Filter for feb
+    movie_ratings_feb = movie_rating.groupby('movie_id', as_index=False)['rating'].mean()  # Find averages
+    movie_ratings_feb = movie_ratings_feb.merge(movies)  # Add names column
+    #  In case of a tie, return the lexicographically smaller movie name.
+    movie_ratings_feb = movie_ratings_feb.sort_values(['rating', 'title'], ascending=[False, True])
+    second_ans = movie_ratings_feb.head(1)['title']
+
+    # Combine into one DF
+    res = pd.concat([first_ans, second_ans]).to_frame().rename(columns={0: 'results'})
+
+    return res
