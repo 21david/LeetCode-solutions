5271c12b6ddf696fd03f9f65a07f3e6c6198144d
David <david.espinosa.21@gmail.com>
2026-04-04 18:03:40 -0500

    Update Tree Node.py

--- Pandas/Medium/Tree Node.py
+++ Pandas/Medium/Tree Node.py
@@ -1,5 +1,41 @@
 import pandas as pd
 
+# Approach: Create a function and use apply()
+def tree_node(tree: pd.DataFrame) -> pd.DataFrame:
+    parents = set(tree.p_id.unique().dropna())
+
+    def get_type(row):
+        if pd.isna(row.p_id):
+            return 'Root'
+        elif row.id in parents:
+            return 'Inner'
+        return 'Leaf'
+
+    tree['type'] = tree.apply(get_type, axis=1)  # axis=1 sends each row instead of columns
+
+    return tree[['id', 'type']]
+
+# -----------------------------------------------------
+
+# Approach: Using df.loc[ <condition aka mask>, <column> ] = <new value>
+def tree_node(tree: pd.DataFrame) -> pd.DataFrame:
+    # Get unique set of parents
+    parents = set(tree.p_id.unique().dropna())
+
+    # Set all as Leaf. Parents and Root will be overwritten later.
+    tree['type'] = 'Leaf'
+
+    # Set nodes that are parents as inner nodes
+    tree.loc[ tree['id'].isin(parents) , 'type'] = 'Inner'
+
+    # Set the one root node (no parent) as root. This needs to be done last because
+    # it is also just a parent, so the line above would overwrite this as 'Inner' if done after
+    tree.loc[ tree['p_id'].isnull(), 'type'] = 'Root'
+
+    return tree[['id', 'type']]
+
+# -----------------------------------------------------
+
 # Experiment using Python features, beats 91%+
 def tree_node(tree: pd.DataFrame) -> pd.DataFrame:
     # Uniques in p_id gives all parent nodes
