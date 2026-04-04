cbe1dd9f2d118669cb9a441187cb40a71805d88d
David <david.espinosa.21@gmail.com>
2026-04-04 15:56:23 -0500

    Update Students and Examinations.sql

--- SQL/Easy/Students and Examinations.sql
+++ SQL/Easy/Students and Examinations.sql
@@ -8,3 +8,33 @@ select s.student_id, s.student_name, sub.subject_name, ifnull(counts.a_e, 0) as 
 cross join subjects sub
 left join counts on s.student_id = counts.student_id and sub.subject_name = counts.subject_name
 order by student_id, subject_name
+
+
+
+# Another solution:
+-- Get all possible combinations of students and subjects
+with combinations as (
+    select student_id, student_name, subject_name
+    from students st
+    cross join subjects sb
+),
+
+-- Get counts of each subject test taken by each student
+test_counts as (
+    select student_id, subject_name, count(*) as counts
+    from examinations
+    group by student_id, subject_name
+)
+
+-- Left join to match counts of each subject taken to all possible combinations.
+-- For resulting null values (student didnt take even 1 of that test), use ifnull() to make it 0
+select 
+    combinations.student_id, 
+    student_name, 
+    combinations.subject_name, 
+    ifnull(test_counts.counts, 0) as attended_exams
+from combinations
+left join test_counts
+    on combinations.student_id = test_counts.student_id
+    and combinations.subject_name = test_counts.subject_name
+order by combinations.student_id, combinations.subject_name
